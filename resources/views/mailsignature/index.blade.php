f2c270904f6936449bdb7f937360d417bab58abc
sathish <you@example.com>
2020-10-01 14:47:54 +0900

    image add

--- resources/views/mailsignature/index.blade.php
+++ resources/views/mailsignature/index.blade.php
@@ -22,10 +22,50 @@
 <script type="text/javascript">
 	var mainmenu = '@php echo $request->mainmenu; @endphp';
 	var datetime = '@php echo date("Ymdhis") @endphp';
+	function pageClick(pageval) {
+		$('#page').val(pageval);
+		$("#mailsignaturefrm").submit();
+	}
+	function pageLimitClick(pagelimitval) {
+		$('#page').val('');
+		$('#plimit').val(pagelimitval);
+		$("#mailsignaturefrm").submit();
+	}
 </script>
+{{ Form::open(array('name'=>'mailsignaturefrm',
+		'id'=>'mailsignaturefrm',
+		'url' => 'MailSignature/index?mainmenu='.$request->mainmenu.'&time='.date('YmdHis'), 
+		'method' => 'POST')) }}
 <div class="" id="main_contents">
 	<!-- article to select the main&sub menu -->
 	<article id="mail" class="DEC_flex_wrapper" data-category="mail mail_sub_2">
+		<!-- Start Heading -->
+		<fieldset class="pm0 mt20">
+			<div class="header">
+				<img class="headerimg box40 imgviewheight" src="{{ URL::asset('public/images/signature.png')  }}">
+				<h2 class="pull-left pl5 mt10">{{ trans('messages.lbl_mailsignature') }}</h2>
+				</h2>
+			</div>
+		</fieldset>
+		<!-- End Heading -->
+		<!-- Session msg Start-->
+		@if(Session::has('success'))
+			<div align="center" class="alertboxalign" role="alert">
+				<p class="alert {{ Session::get('alert', Session::get('type') ) }}">
+				{{ Session::get('success') }}
+				</p>
+			</div>
+		@endif
+		@if(Session::has('danger'))
+			<div align="center" class="alertboxalign" role="alert">
+				<p class="alert {{ Session::get('alert', Session::get('type') ) }}">
+				{{ Session::get('danger') }}
+				</p>
+			</div>
+		@endif
+		@php Session::forget('success'); @endphp
+		@php Session::forget('danger'); @endphp
+		<!-- Session msg End-->
 	</article>
 </div>
 @endsection
